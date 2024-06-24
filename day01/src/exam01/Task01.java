4003e8de751ef7493b1d10c47a036dc21560e691
Kim Kyungyun <153585851+20204376@users.noreply.github.com>
2024-06-24 22:10:50 +0900

    Update Task01.java

--- day01/src/exam01/Task01.java
+++ day01/src/exam01/Task01.java
@@ -9,7 +9,8 @@ public class Task01 {
 //		여러줄 주석 해제 단축기 : cn + shi + \
 //		System.out.println("안녕하세요");   ,(출력 메소드)
 //		ctrl + f11 : 실행 강축기
-		System.out.println("안녕하세요");
+		System.out.println("안녕하세요")
 	
 	}
+}
 }
